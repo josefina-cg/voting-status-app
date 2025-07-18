18a74e9fc52f295e34d2ac399acec3a103cda3bd
josefina-cg <josefinac@dgo-on.com>
2025-07-18 12:39:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,8 @@ st.markdown("""
 
     <div class="footer">
         <img src="https://raw.githubusercontent.com/josefina-cg/voting-status-app/main/BallotBox%20Logo.png" alt="Logo">
-        <span>¿Necesitas ayuda? Contáctanos al Whatsapp: <a href="https://wa.me/56923987722" target="_blank">+56 9 2398 7722</a></span>
+        <span>¿Necesitas ayuda?<br>Contáctanos al Whatsapp: <a href="https://wa.me/56923987722" target="_blank">+56 9 2398 7722</a></span>
+
     </div>
 """, unsafe_allow_html=True)
 
